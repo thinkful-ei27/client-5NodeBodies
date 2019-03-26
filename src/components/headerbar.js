6fba99e0483a53da4a065e253daeb1b052a065ca
Jordan Heffernan <jheff2@gmail.com>
2019-03-26 12:25:06 -0700

    built nav-buttons and h1, added authActions for logout and TokenRefresh, Need to add reducers for logging out

--- src/components/headerbar.js
+++ src/components/headerbar.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../utils/local-storage';
+// import { Link } from 'react-router-dom';
 
-export default class Headerbar extends React.Component{
-  render(){
+export class Headerbar extends React.Component {
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken()
+  }
+
+  render() {
+    // Only render log out button if we are logged in
+    let navButtons;
+    if (this.props.loggedIn) {
+      navButtons = <div className="nav-buttons">
+        {/* TO-DO: REACT-ROUTER INSTEAD???????????????? */}
+        <a className="button" href="/dashboard">
+          <button className="home-button">Home</button>
+        </a>
+        <button type="button" onClick={() => this.logOut()}>Log Out</button>
+      </div>
+    }
     return (
-      <div>This is the header!</div>
+      <header className="headerbar">
+        <h1>This is Our App Title!</h1>
+        {navButtons}
+      </header>
     )
   }
 }
+
+// TO-DO: GOTTA SET THIS TO THAT CORRECT STATE/STORE VALUE
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(Headerbar);
