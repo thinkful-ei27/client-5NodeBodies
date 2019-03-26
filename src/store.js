1fd742a151491cce29d1770fdc34496d593d6eee
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-03-26 10:54:57 -0400

    redux devtools on fleek

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import loginReg from './reducers/loginreg';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
 const store = createStore(combineReducers({loginReg: loginReg}), composeEnhancers(applyMiddleware(thunk)));
