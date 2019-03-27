9ab6cecb992a367ea842a5aa1a9f7d0dfac193d3
Jordan Heffernan <jheff2@gmail.com>
2019-03-27 14:16:41 -0700

    added travis.yml and publish.sh for deployments. added boilerplate nodes actions and reducers

---
+++ src/actions/nodes.js
@@ -0,0 +1,38 @@
+
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from '../utils';
+
+export const CREATE_NODE_REQUEST = 'CREATE_NODE_REQUEST';
+export const createNodeRequest = () => ({
+  type: CREATE_NODE_REQUEST,
+});
+
+export const CREATE_NODE_SUCCESS = 'CREATE_NODE_SUCCESS';
+export const createNodeSuccess = (nodeId) => ({
+  type: CREATE_NODE_SUCCESS,
+  nodeId
+});
+
+export const CREATE_NODE_ERROR = 'CREATE_NODE_ERROR';
+export const createNodeError = error => ({
+  type: CREATE_NODE_ERROR,
+  error
+});
+
+export const createNode = node => (dispatch) => {
+  dispatch(createNodeRequest())
+  return fetch(`${API_BASE_URL}/node`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(node)
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(res => dispatch(createNodeSuccess(res)))
+    .catch(err => {
+      dispatch(createNodeError(err))
+
+    });
+};
