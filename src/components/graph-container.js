1e35ecb11e310732ced4a33c610bd6baa11c03ab
David Johnson <dajohnson1998@gmail.com>
2019-04-01 16:11:36 -0400

    nodes now render with the correct node ids

--- src/components/graph-container.js
+++ src/components/graph-container.js
@@ -4,74 +4,45 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ReactCytoscape } from 'react-cytoscape';
 import { Graph } from 'react-d3-graph';
- 
-// graph payload (with minimalist structure)
-const data = {
-    nodes: [{ id: '4534095834985' }, { id: '345345340958' }, { id: '234234293048' }],
-    links: [{ source: '4534095834985', target: '345345340958' }, { source: '345345340958', target: '234234293048' }]
-};
- 
-// the graph configuration, you only need to pass down properties
-// that you want to override, otherwise default ones will be used
-const myConfig = {
-    nodeHighlightBehavior: true,
-    node: {
-        color: 'lightgreen',
-        size: 120,
-        highlightStrokeColor: 'blue'
-    },
-    link: {
-        highlightColor: 'lightblue'
-    }
-};
-
-
- 
-// graph event callbacks
-// const onClickGraph = function() {
-//     window.alert(`Clicked the graph background`);
-// };
- 
-// const onClickNode = function(nodeId) {
-//     window.alert(`Clicked node ${nodeId}`);
-// };
- 
-// const onRightClickNode = function(event, nodeId) {
-//     window.alert(`Right clicked node ${nodeId}`);
-// };
- 
-// const onMouseOverNode = function(nodeId) {
-//     window.alert(`Mouse over node ${nodeId}`);
-// };
- 
-// const onMouseOutNode = function(nodeId) {
-//     window.alert(`Mouse out node ${nodeId}`);
-// };
- 
-// const onClickLink = function(source, target) {
-//     window.alert(`Clicked link between ${source} and ${target}`);
-// };
- 
-// const onRightClickLink = function(event, source, target) {
-//     window.alert(`Right clicked link between ${source} and ${target}`);
-// };
- 
-// const onMouseOverLink = function(source, target) {
-//     window.alert(`Mouse over in link between ${source} and ${target}`);
-// };
- 
-// const onMouseOutLink = function(source, target) {
-//     window.alert(`Mouse out link between ${source} and ${target}`);
-// };
-
+let data = {
+    nodes: [],
+    links: []
+}
 class GraphContainer extends React.Component{
 
     onClickNode(nodeId) {
         console.log(nodeId);
-        console.log(this.props.SOMETHINGUNIQUE);
+        console.log(this.props.nodez)
     }
-
+    componentWillMount() {
+        console.log(this.props.nodez);
+            for (let i =0; i < this.props.nodez.length; i++) {
+                data.nodes.push({id: this.props.nodez[i].id})
+            }
+            // data = {
+            // nodes: [{ id: '4534095834985' }, { id: '345345340958' }, { id: '234234293048' }],
+            // links: [{ source: '4534095834985', target: '345345340958' }, { source: '345345340958', target: '234234293048' }]
+        // };
+    }
     render(){
+        // const data = {
+        //     nodes: [{ id: '4534095834985' }, { id: '345345340958' }, { id: '234234293048' }],
+        //     links: [{ source: '4534095834985', target: '345345340958' }, { source: '345345340958', target: '234234293048' }]
+        // };
+         
+        // the graph configuration, you only need to pass down properties
+        // that you want to override, otherwise default ones will be used
+        const myConfig = {
+            nodeHighlightBehavior: true,
+            node: {
+                color: 'lightgreen',
+                size: 120,
+                highlightStrokeColor: 'blue'
+            },
+            link: {
+                highlightColor: 'lightblue'
+            }
+        };
         let cyStyle = {
             height: '100vh',
             width: '100vw',
@@ -80,6 +51,7 @@ class GraphContainer extends React.Component{
           return (
             <div style={cyStyle}>
               <Graph
+                props={this.props}
                 id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
                 data={data}
                 config={myConfig}
@@ -99,8 +71,9 @@ class GraphContainer extends React.Component{
 }
 
 const mapStateToProps = state => ({
-    SOMETHINGUNIQUE: state.adventure.currentAdventure.nodes
+    nodez: state.adventure.currentAdventure.nodes
+
 })
 
-
-export default connect(mapStateToProps)(GraphContainer);
+connect(mapStateToProps)(Graph)
+export default  connect(mapStateToProps)(GraphContainer);
