becbbd071f834d22c88ed5a248a40290f97dd052
Jordan Heffernan <jheff2@gmail.com>
2019-04-02 08:56:25 -0700

    updated adventure form for input clarity. added attributes to input and textarea components

--- src/components/newAdventure.js
+++ src/components/newAdventure.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form, Field, reduxForm } from 'redux-form';
 import Input from "./input";
+import TextArea from "./textarea";
 import { createAdventure } from '../actions/createAdventure';
 import { required, nonEmpty } from "../utils/validators";
 import { withRouter, Redirect, Link } from 'react-router-dom';
@@ -46,74 +47,89 @@ class AdventureForm extends React.Component {
         {error}
         <Field
           className="title"
-          label="title"
+          label="Adventure Title"
+          ariaLabel="adventure title"
+          placeholder="LearnVenture"
           name="title"
           component={Input}
           type="text"
           validate={[required, nonEmpty]} />
         <Field
           className="startContent"
-          label="Intro Content"
+          label="Adventure Introduction"
+          ariaLabel="Adventure Introduction"
+          placeholder="This is the beginning of your learning quest. Let's have some fun!"
           name="startContent"
-          component={Input}
+          component={TextArea}
           type="text" />
         <Field
           className="videoURL"
-          label="http://(videoURL)"
+          label="Opening video URL(optional)"
+          ariaLabel="Opening video URL(optional)"
+          placeholder="https://www.youtube.com/embed/dHSQAEam2yc"
           name="startVideoURL"
           component={Input}
           type="text" />
-        <div> You have to create your first question as well</div>
         <Field
           className="question"
-          label="What's your starting question? (the meaning of life)"
+          label="Starting question"
+          placeholder="Where will you start?"
+          ariaLabel="starting question"
           name="question"
           component={Input}
           type="text"
           validate={[required, nonEmpty]} />
         <Field
           className="textContent"
-          label="Text Content for head Node"
+          label="Scenario Description for Starting Node"
+          ariaLabel="Scenario Description for Starting Node"
+          placeholder="You are in a situation that requires a choice. What will you do?"
           name="textContent"
-          component={Input}
+          component={TextArea}
           type="text" />
         <Field
           className="video"
-          label="Video URL for head Node"
+          label="Video URL for Starting Node"
+          ariaLabel="Video URL for Starting Node"
+          placeholder="https://www.youtube.com/embed/Mun1dKkc_As"
           name="videoURL"
           component={Input}
           type="text" />
         <Field
-          className="answer A"
+          className="answerA"
           label="Answer A"
+          ariaLabel="Answer A"
+          placeholder="Turn Right"
           name="answerA"
           component={Input}
           type="text"
           validate={[required, nonEmpty]} />
         <Field
-          className="answer B"
+          className="answerB"
           label="Answer B"
+          ariaLabel="Answer B"
+          placeholder="Turn Left"
           name="answerB"
           component={Input}
           type="text" />
         <Field
-          className="answer C"
+          className="answerC"
           label="Answer C"
+          ariaLabel="Answer C"
+          placeholder="Forge Ahead"
           name="answerC"
           component={Input}
           type="text" />
         <Field
-          className="answer D"
+          className="answerD"
           label="Answer D"
+          ariaLabel="Answer D"
+          placeholder="Turn Back"
           name="answerD"
           component={Input}
           type="text" />
         <button>New Adventure!</button>
       </Form>
-      {/*<Link to="/adventure/adventureBuilder">
-        <button className="">go to node adventure builder</button>
-    </Link>*/}
-
     </div>
     )
   }
