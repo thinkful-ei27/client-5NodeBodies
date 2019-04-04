68f6ba806c9dd75b419412a10e103293afc07b6c
Jordan Heffernan <jheff2@gmail.com>
2019-04-04 12:56:50 -0700

    removed excess methods

--- src/components/adventureBuilder.js
+++ src/components/adventureBuilder.js
@@ -21,20 +21,6 @@ export class AdventureBuilder extends React.Component {
     let node = this.props.currentAdventure.nodes.find(node => node.id === value);
     this.props.dispatch(setCurrentNode(node))
   }
-  
-  displayAdventureDeleting() {
-    return this.props.dispatch(toggleAdventureDeleting())
-  }
-
-  onClickDelete() {
-    let adId = this.props.currentAdventure.id;
-    return this.props.dispatch(deleteAdventure(adId))
-      .then(() => {
-        this.props.dispatch(toggleAdventureDeleting())
-        this.props.history.push('/dashboard')
-
-      })
-  }
 
   render() {
     const adventure = this.props.currentAdventure
