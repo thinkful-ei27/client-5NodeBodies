a7f2c0fb9664e1309b3b8ccb966c0c7265fc643b
Publikwerker <jason.michael.hoffman@gmail.com>
2019-04-05 07:51:27 -0700

    moved embark button, removed adventure ID from student display in adventure, fixed click here to register link

--- src/components/adventureSearch.js
+++ src/components/adventureSearch.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getStudentSearch, getStudentAll } from '../actions/student';
+import { getStudentSearch } from '../actions/student';
 
 
 let inputVal;
