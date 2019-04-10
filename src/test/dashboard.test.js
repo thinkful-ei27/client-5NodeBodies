cfdd5a3e466df860e682d692f82019920a78d5ca
David Johnson <dajohnson1998@gmail.com>
2019-04-10 14:47:19 -0400

    Attempting to simulate click events

--- src/test/dashboard.test.js
+++ src/test/dashboard.test.js
@@ -6,4 +6,13 @@ describe('<Dashboard />', () => {
   it('renders without crashing', () => {
     shallow(<Dashboard />);
   })
+
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = mount((
+      <Dashboard onButtonClick={onButtonClick} />
+    ));
+    wrapper.find('.create-adventure').simulate('click');
+    console.log(onButtonClicks.calledOnce)
+  });
 });
