02c3866c2622c0f3c6f1dceb62adc4ce85220849
nicknotj <nicknotjustin@gmail.com>
2019-04-11 16:18:14 -0500

    deleted combinedAdventure

--- src/test/combinedAdventure.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CombinedAdventure from '../components/combinedAdventure';
-
-describe('<CombinedAdventure />', () => {
-  it('renders without crashing', () => {
-    shallow(<CombinedAdventure />);
-  })
-});
