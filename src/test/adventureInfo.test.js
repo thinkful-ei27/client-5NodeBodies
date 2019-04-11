506f83591e39d78cd1945e2195bbdfbd5f1d2455
nicknotj <nicknotjustin@gmail.com>
2019-04-11 15:45:47 -0500

    making even more tests.

--- src/test/adventureInfo.test.js
+++ src/test/adventureInfo.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import AdventureBuilder from '../components/adventureBuilder';
+import { AdventureInfo } from '../components/adventureInfo';
+import Analytics from '../components/analytics';
 
 describe('<AdventureInfo />', () => {
   it('renders without crashing', () => {
-    shallow(<AdventureBuilder />);
-  })
+    const props = {
+      match: {params: 1},
+      dispatch: jest.fn()
+    }
+    shallow(<AdventureInfo {...props} />);
+  });
+  // it('should contain the <Analytics /> if showAnalytics = true', () => {
+  //   const props = {
+  //     match: {params: 1},
+  //     dispatch: jest.fn(),
+  //     showAnalytics: true,
+  //     adventure: true,
+  //     isEditing: false
+  //   }
+  //   const wrapper = shallow(<AdventureInfo {...props} />);
+  //   expect(wrapper.find(Analytics).length).toBe(1);
+  // })
 });
